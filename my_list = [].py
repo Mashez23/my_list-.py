a51662c8a1065d7333cc0c1a647e0c3d782ca92c
Mashez23 <158018768+Mashez23@users.noreply.github.com>
2024-03-08 08:17:03 -0800

    Add files via upload

---
+++ my_list = [].py
@@ -0,0 +1,24 @@
+my_list = []
+#Append elements into my list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert 15 at the second position
+my_list.insert(1, 15)
+
+# Extend the list with another list
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find the index of 30
+index = my_list.index(30)
+
+# Print the index
+print(index)
